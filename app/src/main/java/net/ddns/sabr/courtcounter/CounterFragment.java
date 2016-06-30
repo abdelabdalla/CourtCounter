b8ad2c627120a6c1068cda082cd0b4efb89ef77a
Abdel Abdalla <abdelabdalla@gmail.com>
2016-06-30 20:42:06 +0100

    Updated App

--- app/src/main/java/net/ddns/sabr/courtcounter/CounterFragment.java
+++ app/src/main/java/net/ddns/sabr/courtcounter/CounterFragment.java
@@ -1,8 +1,8 @@
 package net.ddns.sabr.courtcounter;
 
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,16 +12,16 @@ import android.widget.TextView;
 public class CounterFragment extends Fragment implements View.OnClickListener {
 
 
-    public CounterFragment() {}
+    public CounterFragment() {
+    }
 
     TextView scoreText;
-
-    int score = 0;
 
     Button button1;
     Button button2;
     Button button3;
-    Button resetButton;
+
+    int containerID = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,38 +29,62 @@ public class CounterFragment extends Fragment implements View.OnClickListener {
 
         View root = inflater.inflate(R.layout.fragment_counter, container, false);
 
+        containerID = container.getId();
+
         scoreText = (TextView) root.findViewById(R.id.scoreTextView);
+
+        if (containerID == R.id.frame1)
+            scoreText.setText(String.valueOf(MainActivity.score1));
+        else
+            scoreText.setText(String.valueOf(MainActivity.score2));
 
         button1 = (Button) root.findViewById(R.id.oneButton);
         button2 = (Button) root.findViewById(R.id.twoButton);
         button3 = (Button) root.findViewById(R.id.threeButton);
-        resetButton = (Button) root.findViewById(R.id.resetButton);
 
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
         button3.setOnClickListener(this);
-        resetButton.setOnClickListener(this);
 
         return root;
     }
 
+    void reset(){
+        scoreText.setText(String.valueOf(0));
+    }
+
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.oneButton :
-                score++;
-                scoreText.setText(Integer.toString(score));
+        switch (view.getId()) {
+            case R.id.oneButton:
+                if (containerID == R.id.frame1){
+                    MainActivity.score1++;
+                    scoreText.setText(Integer.toString(MainActivity.score1));
+                }
+                else {
+                    MainActivity.score2++;
+                    scoreText.setText(Integer.toString(MainActivity.score2));
+                }
                 break;
-            case R.id.twoButton :
-                score += 2;
-                scoreText.setText(Integer.toString(score));
+            case R.id.twoButton:
+                if (containerID == R.id.frame1){
+                    MainActivity.score1+=2;
+                    scoreText.setText(Integer.toString(MainActivity.score1));
+                }
+                else {
+                    MainActivity.score2+=2;
+                    scoreText.setText(Integer.toString(MainActivity.score2));
+                }
                 break;
-            case R.id.threeButton :
-                score += 3;
-                scoreText.setText(Integer.toString(score));
-                break;
-            case R.id.resetButton :
-                scoreText.setText(Integer.toString(0));
+            case R.id.threeButton:
+                if (containerID == R.id.frame1){
+                    MainActivity.score1+=3;
+                    scoreText.setText(Integer.toString(MainActivity.score1));
+                }
+                else {
+                    MainActivity.score2+=3;
+                    scoreText.setText(Integer.toString(MainActivity.score2));
+                }
                 break;
             default:
                 break;
--- app/src/main/java/net/ddns/sabr/courtcounter/MainActivity.java
+++ app/src/main/java/net/ddns/sabr/courtcounter/MainActivity.java
@@ -1,13 +1,62 @@
 package net.ddns.sabr.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    static int score1 = 0;
+    static int score2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null){
+            score1 = savedInstanceState.getInt("1");
+            score2 = savedInstanceState.getInt("2");
+        }
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        final CounterFragment fragment = new CounterFragment();
+        Bundle b1 = new Bundle();
+        b1.putInt("n",1);
+        fragment.setArguments(b1);
+        fragmentTransaction.replace(R.id.frame1, fragment);
+        fragmentTransaction.commit();
+
+        FragmentTransaction fragmentTransaction2 = fragmentManager.beginTransaction();
+
+        final CounterFragment fragment2 = new CounterFragment();
+        Bundle b2 = new Bundle();
+        b2.putInt("n",2);
+        fragment.setArguments(b2);
+        fragmentTransaction2.replace(R.id.frame2, fragment2);
+        fragmentTransaction2.commit();
+
+        Button resetButton = (Button) findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                score1 = 0;
+                fragment.reset();
+                score2 = 0;
+                fragment2.reset();
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("1",score1);
+        outState.putInt("2",score2);
+        super.onSaveInstanceState(outState);
     }
 }
